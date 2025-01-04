a7894b1a0e1bbc518c040ebbddc19e66e566077a
Honsage <musatov.i.a@edu.mirea.ru>
2025-01-04 19:18:38 +0300

    feat(slae): add header file with headers of slae methods functions

---
+++ SLAE/slae.h
@@ -0,0 +1,9 @@
+#ifndef SLAE_H
+#define SLAE_H
+
+#include <vector>
+
+std::vector<std::vector<double>> augmentedMatrixRequest();
+std::vector<double> gauss(std::vector<std::vector<double>>& matrix);
+
+#endif //SLAE_H
