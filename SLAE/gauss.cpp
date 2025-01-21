89267604610df439983eb23f85b31ab62793fa4a
Honsage <musatov.i.a@edu.mirea.ru>
2025-01-21 15:23:20 +0300

    refactor(gauss): optimize redundant variables

--- SLAE/gauss.cpp
+++ SLAE/gauss.cpp
@@ -14,18 +14,16 @@
 */
 
 std::vector<std::vector<double>> augmentedMatrixRequest() {
-    int n, m;
+    int n;
     std::cout << "Enter dimension of Augmented Matrix (A|B) of SLAE:\n";
     std::cout << "Enter count of rows:";
     std::cin >> n;
-    std::cout << "Enter count of columns, including B:";
-    std::cin >> m;
 
-    std::vector<std::vector<double>> matrix(n, std::vector<double>(m));
+    std::vector<std::vector<double>> matrix(n, std::vector<double>(n+1));
 
     for (int i = 0; i < n; ++i) {
         std::cout << "Enter row number " << i+1 << ":";
-        for (int j = 0; j < m; ++j) {
+        for (int j = 0; j < n+1; ++j) {
             std::cin >> matrix[i][j];
         }
     }
@@ -33,28 +31,27 @@ std::vector<std::vector<double>> augmentedMatrixRequest() {
     return matrix;
 }
 
-void matrixToTriangularForm(std::vector<std::vector<double>>& matrix, const int& n, const int& m) {
+void matrixToTriangularForm(std::vector<std::vector<double>>& matrix, const int& n) {
     for (int k = 0; k < n; ++k) {
-        for (int j = k + 1; j < m; ++j) matrix[k][j] /= matrix[k][k];
+        for (int j = k + 1; j < n+1; ++j) matrix[k][j] /= matrix[k][k];
         matrix[k][k] = 1;
 
         for (int i = k + 1; i < n; ++i) {
-            for (int j = k + 1; j < m; ++j) matrix[i][j] -= matrix[i][k] * matrix[k][j];
+            for (int j = k + 1; j < n+1; ++j) matrix[i][j] -= matrix[i][k] * matrix[k][j];
             matrix[i][k] = 0;
-
         }
     }
 }
 
 std::vector<double> gauss(std::vector<std::vector<double>>& matrix) {
-    int n = matrix.size(), m = matrix[0].size();
+    int n = matrix.size();
     // 'Row reduction' or 'Forward pass'
-    matrixToTriangularForm(matrix, n, m);
+    matrixToTriangularForm(matrix, n);
     // 'Back-substitution' or 'Reverse pass'
     std::vector<double> X (n);
     for (int i = n-1; i >= 0; --i) {
-        X[i] = matrix[i][m-1];
-        for(int k = n-1; k > i; --k) {
+        X[i] = matrix[i][n];
+        for (int k = n-1; k > i; --k) {
             X[i] -= matrix[i][k] * X[k];
         }
     }
