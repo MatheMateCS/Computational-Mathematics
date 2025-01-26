ed8ac50e06417c495c80831eeb96f01eb350d127
Honsage <musatov.i.a@edu.mirea.ru>
2025-01-26 23:14:55 +0300

    feat(slae): add python implementation of Seidel method

--- SLAE/slae.py
+++ SLAE/slae.py
@@ -3,6 +3,7 @@ from math import *
 
 def gauss(M: list[list]) -> list:
     n = len(M)
+    # 'Row reduction' or 'Forward pass'
     for k in range(0, n, 1):
         for j in range(k+1, n+1, 1):
             M[k][j] /= M[k][k]
@@ -13,6 +14,7 @@ def gauss(M: list[list]) -> list:
                 M[i][j] -= M[i][k] * M[k][j]
             M[i][k] = 0
     X = [0] * n
+    # 'Back-substitution' or 'Reverse pass'
     for i in range(n-1, -1, -1):
         X[i] = M[i][n]
         for k in range(n-1, i, -1):
@@ -20,5 +22,26 @@ def gauss(M: list[list]) -> list:
     return X
 
 
-def seidel():
-    pass
+def seidel(M: list[list], eps: float = 0.001):
+    def max_delta(X: list, X_prev: list) -> float:
+        return max([fabs(X[i] - X_prev[i]) for i in range(len(X))])
+    n = len(M)
+    for i in range(0, n, 1):
+        val = M[i][i]
+        for j in range(0, n+1, 1):
+            M[i][j] /= val
+
+    X, X_prev = [M[i][n] for i in range(0, n, 1)], [] * n
+
+    precision = eps * 10
+    while precision >= eps:
+        X_prev = [X[i] for i in range(len(X))]
+        for i in range(0, n, 1):
+            X[i] = M[i][n]
+            for j in range(0, n, 1):
+                if i == j:
+                    continue
+                X[i] -= M[i][j] * X[j]
+        precision = max_delta(X, X_prev)
+
+    return X
